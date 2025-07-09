bf08416816d518dd8ed5de0ce3b076815bbdfd32
Felipe Mateus <felipe_mateus08@hotmail.com>
2025-07-09 14:28:28 -0300

    feat: first config and nativewind install

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,9 @@
+export const useAuth = (): AuthContextData => {
+  const context = useContext(AuthContext)
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+
+  return context
+}
